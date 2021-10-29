30b820b8f690f93f5c5e138b44901b12bc367aaf
rayauxey <rayauxey@gmail.com>
2021-10-29 05:30:44 +0300

    Ability for user to change a value in the grid

--- main.mjs
+++ main.mjs
@@ -1,5 +1,16 @@
-import logUpdate from "log-update";
 import colors from "colors/safe.js";
+import readline from "readline";
+import { stdin as input, stdout as output } from "process";
+import logUpdate, { createLogUpdate } from "log-update";
+
+const rl = readline.createInterface({
+  input,
+  output,
+});
+
+const log = createLogUpdate(output, {
+  showCursor: true,
+});
 
 const generateRandomGrid = () => {
   const output = [];
@@ -35,7 +46,47 @@ const printGrid = (list) => {
     output += colors.green("+\n");
   }
 
-  logUpdate(output);
+  log(output);
 };
 
-printGrid(generateRandomGrid());
+let sudokuGrid = generateRandomGrid();
+
+console.clear();
+
+while (true) {
+  log.clear();
+  printGrid(sudokuGrid);
+
+  const row = await getInput("Specify row:   ");
+  if (isNumeric(row) && isBound(row, 0, 8)) {
+    const col = await getInput("Specify col:   ");
+    if (isNumeric(col) && isBound(col, 0, 8)) {
+      const value = await getInput("Specify value: ");
+      if (isNumeric(value) && isBound(value, 1, 9)) {
+        sudokuGrid[row][col] = value;
+      }
+    }
+  }
+
+  console.clear();
+}
+
+function getInput(question) {
+  return new Promise((resolve, _) => {
+    rl.question(question, (answer) => resolve(answer));
+  });
+}
+
+function isNumeric(str) {
+  // str = str.trim();
+  if (typeof str != "string") return false; // we only process strings!
+  return (
+    !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
+    !isNaN(parseFloat(str))
+  ); // ...and ensure strings of whitespace fail
+}
+
+function isBound(val, min, max) {
+  // val = val.trim();
+  return val >= min && val <= max;
+}
